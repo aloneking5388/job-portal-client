c8cdb13757563310b885cefa089437b226260ec6
alineking5388 <alineking5388@gmail.com>
2024-01-03 15:42:05 +0530

    setup-navbar

--- job-portal-client/src/components/Navbar.jsx
+++ job-portal-client/src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { NavLink, Link } from 'react-router-dom';
+import { FaBarsStaggered, FaXmark } from "react-icons/fa6";
 
 const Navbar = () => {
-    const [ ismenuOpen, setIsmenuOpen ] = useState(false);
+    const [ ismenuOpen, setIsmenuOpen ] = useState();
 
     const handleMenuTogler = () => {
         setIsmenuOpen(!ismenuOpen)
@@ -48,13 +49,29 @@ const Navbar = () => {
                 }
             </ul>
             <div className="text-base text-primary font-medium space-x-5 hidden lg:block">
-                <Link to="/login" className='py-2 px-5 border rounded'>Log in</Link>
-                <Link to="/sign-up" className='py-2 px-5 border rounded bg-blue text-white'>Sign up</Link>
+                <Link to="/login" className='py-2 px-5 border rounded-xl'>Log in</Link>
+                <Link to="/sign-up" className='py-2 px-5 rounded-xl bg-red-500 text-white'>Sign up</Link>
             </div>
-            <div className="">
-                
+            <div className='md:hidden block'>
+                <button onClick={handleMenuTogler}>
+                    {
+                        ismenuOpen ? <FaXmark className='w-5 h-5 text-primary'/> : <FaBarsStaggered className='w-5 h-5 text-primary'/>
+                    }
+                </button>
             </div>
         </nav>
+        <div className={`px-4 bg-black py-5 rounded-xl ${ismenuOpen ? "" : "hidden"}`}>
+            {navItems.map(({ path, title }) => (
+                <li key={path} className='text-base text-white first:text-white py-1'>
+                    <NavLink to={path}
+                    className={({ isActive }) => 
+                        isActive ? "active" : ""}>
+                        {title}
+                    </NavLink>
+                </li>
+            ))}
+            <li className='text-white py-1'><Link to="/login">Log in</Link></li>
+        </div>
     </header>
   )
 }
