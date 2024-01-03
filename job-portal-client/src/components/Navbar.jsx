8bda69d106ab863551992ca56e12a9ede5e98c9f
alineking5388 <alineking5388@gmail.com>
2024-01-03 14:41:32 +0530

    nevbar setup

---
+++ job-portal-client/src/components/Navbar.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { NavLink, Link } from 'react-router-dom';
+
+const Navbar = () => {
+    const [ ismenuOpen, setIsmenuOpen ] = useState(false);
+
+    const handleMenuTogler = () => {
+        setIsmenuOpen(!ismenuOpen)
+    };
+
+    const navItems = [
+        {path: "/",  title: "Start a search"},
+        {path: "/my-job",  title: "My Jobs"},
+        {path: "/salary",  title: "Salary Estimate"},
+        {path: "/post-job",  title: "Post A Job"},
+    ]
+
+  return (
+    <header className='max-w-screen-2xl container mx-auto xl:px-24 px-4'>
+        <nav className='flex justify-between items-center py-6'>
+            <a href="/" className='flex items-center gap-2 text-2xl text-black'>
+                <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width="29"
+                height="30"
+                viewBox='0 0 29 30'
+                fill='none'
+                >
+                    <circle cx="12.0143" cy="12.5143" r="12.0143" fill="#ff3648" fillOpacity="0.4" />
+                    <circle cx="16.9857" cy="17.4857" r="12.0143" fill="#ff3648"/>
+                </svg>
+                <span>KaamYab Job Portal</span>
+            </a>
+            <ul className='hidden md:flex gap-12'>
+                {
+                    navItems.map(({path, title}) => (
+                        <li key={path} className='text-base text-primary'>
+                            <NavLink
+                            to={path}
+                            className={({ isActive }) =>
+                            isActive ? "active" : ""
+                        }
+                    >     
+                            {title}
+                            </NavLink>
+                        </li>
+                    ))
+                }
+            </ul>
+            <div className="text-base text-primary font-medium space-x-5 hidden lg:block">
+                <Link to="/login" className='py-2 px-5 border rounded'>Log in</Link>
+                <Link to="/sign-up" className='py-2 px-5 border rounded bg-blue text-white'>Sign up</Link>
+            </div>
+            <div className="">
+                
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
